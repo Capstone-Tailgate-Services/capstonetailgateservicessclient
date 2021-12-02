bd148fd687f4566faf1e645dc1f225b27479d861
Joshua Wiggins <jwiggins1@crimson.ua.edu>
2021-12-01 22:09:22 -0600

    admin portal new background

--- resources/scripts/signup.js
+++ resources/scripts/signup.js
@@ -18,28 +18,7 @@ function getUsers(){
         console.log(error);
     });
 }
-//method that creates an object from the user data and calls the back end and sends the object across to save the user
-/*function putUser(id){
-    const putUserApiUrl = baseUrl + "/"+id;
-    const sendUser = {
-        id: id,
-        userEmail: document.getElementById("useremail").value,
-        userPassword: document.getElementById("userpassword").value
-    }
-    fetch(putUserApiUrl, {
-        method: "PUT",
-        headers: {
-            "Accept": 'application/json',
-            "Content-Type": 'application/json',
-        },
-        body: JSON.stringify(sendUser)
-    })
-    .then((response)=>{
-        myUser = sendUser;
-        getUsers();
-        populateForm();
-    });
-}*/
+
 //method that allows you to save a new user that didn't exist before
 function postUser(){
     const postUserApiUrl = baseUrl;
