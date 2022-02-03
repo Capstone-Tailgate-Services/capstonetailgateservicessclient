67cf510f2739ecb16cd3b623f56586658230d981
dcmeisenheimer <dcmeisenheimer@crimson.ua.edu>
2022-02-03 17:24:22 -0600

    Edited Review System

--- resources/scripts/api.js
+++ resources/scripts/api.js
@@ -3,8 +3,14 @@ var reviewList = [];
 var myReview = {};
 
 function populateList(){ //populates review with all fields
-
     const allReviewApiUrl = baseUrl;
+    var i = 0;
+    var oneStar = 0;
+    var twoStar = 0;
+    var threeStar = 0;
+    var fourStar = 0;
+    var fiveStar = 0;
+    var averageRating = 0;
     fetch(allReviewApiUrl).then(function(response){
         return response.json();
     }).then(function(json){
@@ -12,7 +18,57 @@ function populateList(){ //populates review with all fields
         let html = "<select class = \"listBox\" onchange = \"handleOnChange()\" id= \"selectListBox\" name = \"list_box\" size=5 width=\"100%\">";
         json.forEach((review)=>{
             html += "<option value = " + review.id  + ">" + review.reviewstext  +  "</option>";
+            if(review.reviewsrating == 1){
+                averageRating = averageRating + parseInt(review.reviewsrating);
+                i++;
+                oneStar++;
+                const element = document.getElementById("one-star-count")
+                element.innerHTML = oneStar;
+                review.reviewsrating = "⭐";
+            }
+            else if(review.reviewsrating == 2){
+                averageRating = averageRating + parseInt(review.reviewsrating);
+                i++;
+                twoStar++;
+                const element = document.getElementById("two-star-count")
+                element.innerHTML = twoStar;
+                review.reviewsrating = "⭐⭐";
+            }
+            else if(review.reviewsrating == 3){
+                averageRating = averageRating + parseInt(review.reviewsrating);
+                i++;
+                threeStar++;
+                const element = document.getElementById("three-star-count")
+                element.innerHTML = threeStar;
+                review.reviewsrating = "⭐⭐⭐";
+            }
+            else if(review.reviewsrating == 4){
+                averageRating = averageRating + parseInt(review.reviewsrating);
+                i++;
+                fourStar++;
+                const element = document.getElementById("four-star-count")
+                element.innerHTML = fourStar;
+                review.reviewsrating = "⭐⭐⭐⭐";
+            }
+            else if(review.reviewsrating == 5){
+                averageRating = averageRating + parseInt(review.reviewsrating);
+                i++;
+                fiveStar++;
+                const element = document.getElementById("five-star-count")
+                element.innerHTML = fiveStar;
+                review.reviewsrating = "⭐⭐⭐⭐⭐";
+            }
         })
+        const element = document.getElementById("average-rating")
+        averageRating = averageRating / i;
+        element.innerHTML = averageRating.toFixed(1) + " average based on " + i + " reviews";
+
+        document.getElementById("bar-5").style.width = (fiveStar / i) * 100 + "%";
+        document.getElementById("bar-4").style.width = (fourStar / i) * 100 + "%";
+        document.getElementById("bar-3").style.width = (threeStar / i) * 100 + "%";
+        document.getElementById("bar-2").style.width = (twoStar / i) * 100 + "%";
+        document.getElementById("bar-1").style.width = (oneStar / i) * 100 + "%";
+        
         html += "</select>";
         document.getElementById("listBox").innerHTML = html;
     }).catch(function(error){
